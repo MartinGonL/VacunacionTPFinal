4b6b7d380c3284572f3f2b93dcacf41c63778f4b
MartinGonL <martingontero@hotmail.com>
2026-07-29 19:40:15 -0300

    carga de imagenes

--- Models/IRespositorioAlumno.cs
+++ Models/IRespositorioAlumno.cs
@@ -4,7 +4,7 @@ public interface IRepositorioAlumno
     int Baja(int id);
     int Modificar(Alumno alumno);
     IEnumerable<Alumno> ObtenerTodos();
-    // Paginado para la consigna
+    // Paginado 
     IEnumerable<Alumno> ObtenerPaginados(int pagina, int cantidad);
     int ObtenerTotal();
     Alumno? ObtenerPorId(int id);
